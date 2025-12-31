52f2fef64674f5f7ee4fb49d613dcf2436cafbc6
NoTUrBusIneSs <ideaout3@gmail.com>
2025-12-31 16:42:49 +0100

    Remove SSL for Railway internal database

--- backend/src/store/sequelize.js
+++ backend/src/store/sequelize.js
@@ -6,12 +6,6 @@ let sequelize;
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   });
 } else {
   sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
